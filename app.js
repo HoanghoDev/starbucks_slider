2e09aabef043ef61cf35b0ef5c6320d7b9c962e5
h_ho <hohoang.dev@gmail.com>
2024-02-17 18:53:02 +0700

    updtae

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-
 let next = document.getElementById('next');
 let prev = document.getElementById('prev');
 let carousel = document.querySelector('.carousel');
-let items = document.querySelectorAll('.carousel .list .item');
+let items = document.querySelectorAll('.carousel .item');
+let countItem = items.length;
 let active = 1;
 let other_1 = null;
 let other_2 = null;
-let countItem = items.length;
 next.onclick = () => {
     carousel.classList.remove('prev');
     carousel.classList.add('next');
-    active = active + 1 >= countItem ? 0 : active + 1;
-    other_1 = active - 1 < 0 ? countItem - 1 : active - 1;
+    active =active + 1 >= countItem ? 0 : active + 1;
+    other_1 =active - 1 < 0 ? countItem -1 : active - 1;
     other_2 = active + 1 >= countItem ? 0 : active + 1;
     changeSlider();
 }
@@ -24,14 +23,13 @@ prev.onclick = () => {
     changeSlider();
 }
 const changeSlider = () => {
-
-    let itemOldActive = document.querySelector('.item.active');
+    let itemOldActive = document.querySelector('.carousel .item.active');
     if(itemOldActive) itemOldActive.classList.remove('active');
 
-    let itemOldOther_1 = document.querySelector('.item.other_1');
+    let itemOldOther_1 = document.querySelector('.carousel .item.other_1');
     if(itemOldOther_1) itemOldOther_1.classList.remove('other_1');
 
-    let itemOldOther_2 = document.querySelector('.item.other_2');
+    let itemOldOther_2 = document.querySelector('.carousel .item.other_2');
     if(itemOldOther_2) itemOldOther_2.classList.remove('other_2');
 
     items.forEach(e => {
@@ -45,5 +43,12 @@ const changeSlider = () => {
     items[active].classList.add('active');
     items[other_1].classList.add('other_1');
     items[other_2].classList.add('other_2');
-}
 
+    clearInterval(autoPlay);
+    autoPlay = setInterval(() => {
+        next.click();
+    }, 5000);
+}
+let autoPlay = setInterval(() => {
+    next.click();
+}, 5000);
